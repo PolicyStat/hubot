b6901c56f724ec70f3bf35c3cf5385a02b9cb76e
Kyle Gibson <kyle.gibson@frozenonline.com>
2018-03-19 17:01:41 -0400

    better provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -4,4 +4,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "bento/ubuntu-16.04"
   config.vm.provision "shell", privileged: false, path: "vagrant_provision.sh"
+  config.vm.synced_folder "", "/home/vagrant/hubot"
 end
--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -3,13 +3,9 @@ sudo apt-get update
 sudo apt-get install -y nodejs npm
 # Needed for hipchat
 sudo apt-get install -y libexpat1-dev
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo ln -snf /usr/bin/nodejs /usr/bin/node
 sudo npm install -g yo generator-hubot
 
-mkdir node_modules
-ln -s /vagrant hubot
-ln -s $(pwd)/node_modules hubot/node_modules
-
-pushd hubot
+ln -snf hubot/package.json .
 npm install
-popd
+ln -snf ../node_modules hubot/
